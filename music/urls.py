e7f9aef619dfec9895e21bf003e630a4cbea3247
Mark Wingerd <mawingerd@gmail.com>
2019-10-31 19:24:28 -0700

    Remove the music project

--- music/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<artist_id>[0-9]+)/$', views.detail, name='detail'),
-]
